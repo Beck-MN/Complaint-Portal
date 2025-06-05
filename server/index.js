9f5220f21157a698b96a8407e5f2127ca94ac3e6
Beck-MN <Beck.christensen@gmail.com>
2025-06-05 14:45:08 -0500

    Mongo DB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
-
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { getThoughts, addThought } = require('./handlers/thoughtHandler');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +21,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Log all incoming requests
 app.use((req, res, next) => {
@@ -66,6 +75,9 @@ app.get('/api/thoughts', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch thoughts' });
   }
 });
+
+// Routes
+app.use('/api/complaints', require('./routes/complaints'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
